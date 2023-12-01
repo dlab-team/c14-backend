798862748f570a0bc99987818bcee8f5bd74b4e5
Benja <benjamin_valdes@hotmail.com>
2023-12-01 16:11:05 -0300

    configured db folders and .sequelizerc

---
+++ src/db/config.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    hostServer: process.env.APP_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.DATABASE_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: 'postgres',
+    dialectOptions: {},
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.DATABASE_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
