bd570c30db6ce1f88a7a73cb182f6b2e67587f8a
Carlos Palomo <cepalomos@outlook.es>
2023-12-11 18:03:02 -0500

    fix route polynomial

--- src/routes/polynomial.routes.ts
+++ src/routes/polynomial.routes.ts
@@ -3,15 +3,11 @@ import Middlewares from '@/middlewares';
 import polynomialController from '@/controllers/polynomial.controller';
 const router = Router();
 
-router
-  .route('/')
-  .post(Middlewares.validateResult, polynomialController.createPolynomial)
-  .put(Middlewares.validateResult, polynomialController.putPolynomial)
-  .get(polynomialController.getAllPolynomials);
+router.delete('/:id', Middlewares.validateResult, polynomialController.deletePolynomial);
+router.get('/:id', Middlewares.validateResult, polynomialController.getPolynomialsId);
 
-router
-  .route('/:id')
-  .delete(Middlewares.validateResult, polynomialController.deletePolynomial)
-  .get(Middlewares.validateResult, polynomialController.getPolynomialsId);
+router.post('/', Middlewares.validateResult, polynomialController.createPolynomial);
+router.put('/', Middlewares.validateResult, polynomialController.putPolynomial);
+router.get('/', polynomialController.getAllPolynomials);
 
 export default router;
