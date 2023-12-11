96ad2dec85662d6efeb8aea6492cdd4eec6e9fb4
José Campos <josecamposh95@gmail.com>
2023-12-11 10:29:37 -0300

    add swagger types

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,11 +1,13 @@
+import swaggerJSDoc from 'swagger-jsdoc';
 import __dirname from '@/utils';
 
-const swaggerOptions = {
+const swaggerOptions: swaggerJSDoc.Options = {
   definition: {
     openapi: '3.0.1',
     info: {
       title: 'Laboratorio Civico',
       description: 'Documentación del Backend de la aplicación Laboratorio Civico',
+      version: '1.0',
     },
   },
   apis: [`${__dirname}/docs/*.yaml`],
