258cae0f362ddcd8f01db3007c94bc9f97f7031a
Benja <benjamin_valdes@hotmail.com>
2023-12-15 14:39:32 -0300

    polynomial_option model created

---
+++ src/db/models/polynomial_option.ts
@@ -0,0 +1,59 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+import { sequelize } from '.';
+import { Polynomial } from './polynomial';
+
+export interface PolynomialOptionAttributes {
+  id: string;
+  name: string;
+  group: Array<string>;
+  polynomialId: string;
+}
+
+export interface PolynomialOptionCreationAttributes
+  extends Optional<PolynomialOptionAttributes, 'id'> {}
+
+export interface PolynomialOptionInstance
+  extends Model<PolynomialOptionAttributes, PolynomialOptionCreationAttributes>,
+    PolynomialOptionAttributes {
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export const PolynomialOption = sequelize.define<PolynomialOptionInstance>(
+  'polynomial_option',
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      unique: true,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.TEXT,
+    },
+    group: {
+      allowNull: false,
+      type: DataTypes.ARRAY(DataTypes.ENUM('Extremo1', 'Extremo2', 'Neutro')),
+      defaultValue: [],
+    },
+    polynomialId: {
+      allowNull: false,
+      type: DataTypes.UUID,
+      references: {
+        model: 'polynomial',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    },
+  },
+  {
+    tableName: 'polynomial_option',
+  },
+);
+
+Polynomial.hasMany(PolynomialOption, { foreignKey: 'polynomialId' });
+PolynomialOption.belongsTo(Polynomial, { foreignKey: 'polynomialId' });
