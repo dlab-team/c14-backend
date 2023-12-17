37f37c881626623b61303778d5bf5143e4b7d16e
Lion-Fernandez <contactomoconaguazu@gmail.com>
2023-12-17 17:09:45 -0300

    fixe-phrases-model&migration

--- src/db/migrations/20231217192535-add_phrases_table.ts
+++ src/db/migrations/20231217192535-add_phrases_table.ts
@@ -1,52 +1,51 @@
 'use strict';
 
-import { QueryInterface, DataTypes } from "sequelize";
+import { QueryInterface, DataTypes } from 'sequelize';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    up: async (queryInterface: QueryInterface, Sequelize: typeof DataTypes) => {
-      await queryInterface.createTable('phrases', {
-        id: {
-          allowNull: false,
-          autoIncrement: false,
-          primaryKey: true,
-          type: Sequelize.UUID,
-          defaultValue: Sequelize.UUIDV4,
-          unique: true,
+  up: async (queryInterface: QueryInterface, Sequelize: typeof DataTypes) => {
+    await queryInterface.createTable('phrases', {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        unique: true,
+      },
+      text: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      group: {
+        allowNull: false,
+        type: Sequelize.ENUM('Extremo 1', 'Extremo 2', 'Neutro'),
+      },
+      polynomial_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUID,
+        references: {
+          model: 'polynomial',
+          key: 'id',
         },
-        text: {
-          allowNull: false,
-          type: Sequelize.TEXT,
-        },
-        group: {
-          allowNull: false,
-          type: Sequelize.ENUM('Extremo 1', 'Extremo 2', 'Neutro'),
-        },
-        polynomial_id: {
-          allowNull: false,
-          primaryKey: true,
-          type: Sequelize.UUID,
-          defaultValue: Sequelize.UUID,
-          references: {
-            model: 'polynomial',
-            key: 'id',
-          },
-          onUpdate: 'CASCADE',
-          onDelete: 'CASCADE',
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-      });
-    },
-  
-    down: async (queryInterface: QueryInterface) => {
-      await queryInterface.dropTable('phrases');
-    },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface: QueryInterface) => {
+    await queryInterface.dropTable('phrases');
   },
 };
