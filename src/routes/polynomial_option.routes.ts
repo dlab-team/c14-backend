ea6f424826e650e8cd59f1a93a52a53c8dd88911
José Campos <josecamposh95@gmail.com>
2023-12-20 21:26:24 -0300

    remove validateResult

--- src/routes/polynomial_option.routes.ts
+++ src/routes/polynomial_option.routes.ts
@@ -1,16 +1,11 @@
 import { Router } from 'express';
-import * as Middlewares from '@/middleware';
 import polynomialOptionController from '@/controllers/polynomial_option.controller';
 const router = Router();
 
 router.get('/', polynomialOptionController.getAllPolynomialsOption);
-router.get('/:id', Middlewares.validateResult, polynomialOptionController.getPolynomialsOptionId);
-router.put('/:id', Middlewares.validateResult, polynomialOptionController.putPolynomialOption);
-router.post('/', Middlewares.validateResult, polynomialOptionController.createPolynomialOption);
-router.delete(
-  '/:id',
-  Middlewares.validateResult,
-  polynomialOptionController.deletePolynomialOption,
-);
+router.get('/:id', polynomialOptionController.getPolynomialsOptionId);
+router.put('/:id', polynomialOptionController.putPolynomialOption);
+router.post('/', polynomialOptionController.createPolynomialOption);
+router.delete('/:id', polynomialOptionController.deletePolynomialOption);
 
 export default router;
