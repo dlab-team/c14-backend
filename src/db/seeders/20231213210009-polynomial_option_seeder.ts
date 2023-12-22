169e757ffa40eb9818f8676fe53355d8cc9e8b67
Benja <benjamin_valdes@hotmail.com>
2023-12-22 18:37:14 -0300

    modified polynomial options seeder with real data

--- src/db/seeders/20231213210009-polynomial_option_seeder.ts
+++ src/db/seeders/20231213210009-polynomial_option_seeder.ts
@@ -1,21 +1,420 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use strict';
 
 import { QueryInterface } from 'sequelize';
 import { v4 as uuidv4 } from 'uuid';
-import { faker } from '@faker-js/faker';
+import { Polynomial } from '../models/polynomial';
 
 module.exports = {
   up: async (queryInterface: QueryInterface) => {
-    const polynomialIdTable = await queryInterface.sequelize.query('SELECT id from polynomial;');
+    const results = await Polynomial.findAll({
+      attributes: ['id', 'name'],
+    });
 
-    const polynomialOptions = polynomialIdTable[0].map((polynomial: any) => ({
-      id: uuidv4(),
-      name: faker.lorem.sentence(),
-      group: faker.helpers.arrayElement(['Extremo1', 'Extremo2', 'Neutro']),
-      polynomialId: polynomial.id,
-      createdAt: new Date(),
-      updatedAt: new Date(),
+    const dataArray = results.map(result => ({
+      id: result.getDataValue('id'),
+      name: result.getDataValue('name'),
     }));
+
+    let politicalOption: any[] = [];
+    let genderOption: any[] = [];
+    let regionOption: any[] = [];
+    let incomeOption: any[] = [];
+    let indigenousOption: any[] = [];
+    let religionOption: any[] = [];
+    let immigrationOption: any[] = [];
+    let managementOption: any[] = [];
+    let ageOption: any[] = [];
+    let sexualOrientationOption: any[] = [];
+
+    for (let i = 0; i < dataArray.length; i++) {
+      if (dataArray[i].name === 'Político') {
+        politicalOption = [
+          {
+            id: uuidv4(),
+            name: 'Derecha',
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Izquierda',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Centro',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Independiente',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+      }
+      if (dataArray[i].name === 'Género') {
+        genderOption = [
+          {
+            id: uuidv4(),
+            name: 'Masculino',
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Femenino',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Otro',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+      }
+      if (dataArray[i].name === 'Regiones') {
+        const regions = [
+          'Antofagasta',
+          'Arica y Parinacota',
+          'Atacama',
+          'Aysén',
+          'Biobío',
+          'Coquimbo',
+          'El Maule',
+          'La Araucanía',
+          'Los Lagos',
+          'Los Ríos',
+          'Magallanes',
+          'Ñuble',
+          'O’Higgins',
+          'Tarapacá',
+          'Valparaíso',
+        ];
+
+        for (let j = 0; j < 15; j++) {
+          regionOption.push({
+            id: uuidv4(),
+            name: regions[j],
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          });
+        }
+        const regionOption1 = [
+          {
+            id: uuidv4(),
+            name: 'Metropolitana',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+        regionOption = regionOption.concat(regionOption1);
+      }
+      if (dataArray[i].name === 'Ingresos') {
+        incomeOption = [
+          {
+            id: uuidv4(),
+            name: 'Ingresos altos',
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Ingresos medio altos',
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Ingresos medios',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Ingresos medio bajos',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Ingresos bajos',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+      }
+      if (dataArray[i].name === 'Pueblos indígenas') {
+        const indigenous = [
+          'Atacameño',
+          'Aymara',
+          'Chango',
+          'Colla',
+          'Diaguita',
+          'Kawésqar',
+          'Mapuche',
+          'Quechua',
+          'Rapa Nui',
+          'Selknam',
+          'Yagán',
+          'Otro',
+        ];
+
+        for (let j = 0; j < 12; j++) {
+          indigenousOption.push({
+            id: uuidv4(),
+            name: indigenous[j],
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          });
+        }
+        const indigenousOption1 = [
+          {
+            id: uuidv4(),
+            name: 'No me considero perteneciente a ningún pueblo originario o indígena',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+        indigenousOption = indigenousOption.concat(indigenousOption1);
+      }
+      if (dataArray[i].name === 'Credo') {
+        religionOption = [
+          {
+            id: uuidv4(),
+            name: 'Creyente',
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Ateo',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Agnóstico',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Sin Religión',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+      }
+      if (dataArray[i].name === 'Inmigración') {
+        immigrationOption = [
+          {
+            id: uuidv4(),
+            name: 'Inmigrante',
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Chileno',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+      }
+      if (dataArray[i].name === 'Directivo') {
+        managementOption = [
+          {
+            id: uuidv4(),
+            name: 'Ocupo un cargo directivo en la empresa u organización donde trabajo',
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'No tengo un rol directivo en la empresa u organización donde trabajo',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Soy independiente y trabajo solo. No trabajo en ninguna empresa u organización',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'En este momento no tengo trabajo remunerado (soy estudiante, jubilado, desempleado, labores del hogar, etc.)',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+      }
+      if (dataArray[i].name === 'Etario') {
+        ageOption = [
+          {
+            id: uuidv4(),
+            name: '29 años o menos',
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Entre 30 y 59 años',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: '60 años o más',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+      }
+      if (dataArray[i].name === 'Orientación sexual') {
+        sexualOrientationOption = [
+          {
+            id: uuidv4(),
+            name: 'Heterosexual',
+            group: 'Extremo1',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Gay',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Lesbiana',
+            group: 'Extremo2',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Bisexual',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Pansexual',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Asexual',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Queer',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: uuidv4(),
+            name: 'Otra',
+            group: 'Neutro',
+            polynomialId: dataArray[i].id,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+      }
+    }
+    const polynomialOptions = politicalOption.concat(
+      genderOption,
+      regionOption,
+      incomeOption,
+      indigenousOption,
+      religionOption,
+      immigrationOption,
+      managementOption,
+      ageOption,
+      sexualOrientationOption,
+    );
     await queryInterface.bulkInsert('polynomial_option', polynomialOptions, {});
   },
 
