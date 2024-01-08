88b27400efa06e4b35c5d719aee05afa39f8028f
Benja <benjamin_valdes@hotmail.com>
2024-01-08 09:59:31 -0300

    added route to GET all political phrases

--- src/routes/phrases.routes.ts
+++ src/routes/phrases.routes.ts
@@ -4,11 +4,11 @@ const router = Router();
 
 router.get('/', phrasesController.getAllPhrases);
 router.get('/polynomial/:polynomialId', phrasesController.getPolynomialPhrases);
+router.get('/political/:group', phrasesController.getPoliticalPhrases);
+router.get('/political/', phrasesController.getAllPoliticalPhrases);
 router.get('/:id', phrasesController.getPhrasesId);
 router.put('/:id', phrasesController.putPhrases);
 router.post('/', phrasesController.createPhrases);
 router.delete('/:id', phrasesController.deletePhrases);
-
-router.get('/political/:group', phrasesController.getPoliticalPhrases);
 
 export default router;
