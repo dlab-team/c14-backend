57c3f0e96cbe15e55d764369dda7866356746f00
Daniel Machado <needforock@gmail.com>
2024-01-09 09:40:05 -0300

    fix: added auth to edit/delete/create resurces

--- src/routes/phrases.routes.ts
+++ src/routes/phrases.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import phrasesController from '@/controllers/phrases.controller';
+import { isAuthenticated } from '@/middleware';
 const router = Router();
 
 router.get('/', phrasesController.getAllPhrases);
@@ -7,8 +8,8 @@ router.get('/polynomial/:polynomialId', phrasesController.getPolynomialPhrases);
 router.get('/political/:group', phrasesController.getPoliticalPhrases);
 router.get('/political/', phrasesController.getAllPoliticalPhrases);
 router.get('/:id', phrasesController.getPhrasesId);
-router.put('/:id', phrasesController.putPhrases);
-router.post('/', phrasesController.createPhrases);
-router.delete('/:id', phrasesController.deletePhrases);
+router.put('/:id', isAuthenticated, phrasesController.putPhrases);
+router.post('/', isAuthenticated, phrasesController.createPhrases);
+router.delete('/:id', isAuthenticated, phrasesController.deletePhrases);
 
 export default router;
--- src/routes/polynomial_option.routes.ts
+++ src/routes/polynomial_option.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import polynomialOptionController from '@/controllers/polynomial_option.controller';
+import { isAuthenticated } from '@/middleware';
 const router = Router();
 
 router.get('/', polynomialOptionController.getAllPolynomialsOption);
 router.get('/:id', polynomialOptionController.getPolynomialsOptionId);
-router.put('/:id', polynomialOptionController.putPolynomialOption);
-router.post('/', polynomialOptionController.createPolynomialOption);
-router.delete('/:id', polynomialOptionController.deletePolynomialOption);
+router.put('/:id', isAuthenticated, polynomialOptionController.putPolynomialOption);
+router.post('/', isAuthenticated, polynomialOptionController.createPolynomialOption);
+router.delete('/:id', isAuthenticated, polynomialOptionController.deletePolynomialOption);
 
 export default router;
