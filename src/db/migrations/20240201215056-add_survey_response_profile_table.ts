5157e152a32a3cfc848c317d9a1bf74bd14bc6c8
Lion-Fernandez <contactomoconaguazu@gmail.com>
2024-02-01 19:06:44 -0300

    survey_response_profile

---
+++ src/db/migrations/20240201215056-add_survey_response_profile_table.ts
@@ -0,0 +1,51 @@
+'use strict';
+
+import { DataTypes, QueryInterface } from 'sequelize';
+
+module.exports = {
+  up: async (queryInterface: QueryInterface, Sequelize: typeof DataTypes) => {
+    await queryInterface.createTable('survey_response_profile', {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      surveyResponseId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: 'survey_responses',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      polynomialOptionId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: 'polynomial_options',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  down: async (queryInterface: QueryInterface) => {
+    await queryInterface.dropTable('survey_response_profile');
+  },
+};
