ceff09fe0c179e0c150ba2f13a33fdd8853c1f12
Benja <benjamin_valdes@hotmail.com>
2024-02-05 11:02:11 -0300

    fix: finishDate corrected in survey_response model

---
+++ src/db/models/survey_response.ts
@@ -0,0 +1,44 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '.';
+
+export interface SurveyResponseAttributes {
+  id: string;
+  createdAt: Date;
+  finishDate: Date;
+  duration: number;
+}
+
+export interface SurveyResponseInstance
+  extends Model<SurveyResponseAttributes>,
+    SurveyResponseAttributes {
+  updatedAt?: Date;
+}
+
+export const SurveyResponse = sequelize.define<SurveyResponseInstance>(
+  'survey_response',
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    finishDate: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    duration: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    tableName: 'survey_response',
+  },
+);
