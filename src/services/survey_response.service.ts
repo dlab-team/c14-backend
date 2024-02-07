ef211bb6b732b088e2d9dbb57e0d5ba8496bc181
Daniel Machado <needforock@gmail.com>
2024-02-07 17:00:37 -0300

    feat: get survey metrics

--- src/services/survey_response.service.ts
+++ src/services/survey_response.service.ts
@@ -1,5 +1,6 @@
 import { ServerError } from '@/errors';
 import { SurveyResponse, SurveyResponseAttributes } from '@/db/models/survey_response';
+import { Op } from 'sequelize';
 
 const createResponse = async (
   data: SurveyResponseAttributes,
@@ -12,4 +13,26 @@ const createResponse = async (
   }
 };
 
-export default { createResponse };
+const getMetrics = async () => {
+  try {
+    const unfinished = await SurveyResponse.findAll({
+      attributes: ['id'],
+      where: {
+        finishDate: null,
+      },
+    });
+    const finished = await SurveyResponse.findAll({
+      attributes: ['id'],
+      where: {
+        finishDate: {
+          [Op.not]: null,
+        },
+      },
+    });
+    return { unfinished: unfinished.length, finished: finished.length };
+  } catch (error) {
+    throw new ServerError(error as string, 500);
+  }
+};
+
+export default { createResponse, getMetrics };
